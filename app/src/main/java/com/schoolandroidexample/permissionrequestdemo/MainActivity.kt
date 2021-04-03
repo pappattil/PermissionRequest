abcce48fb35ce69d6340bd94765d76a2e31e79a1
Attila <pappattil@freemail.hu>
2021-04-03 22:07:31 +0200

    PermissionRequest

---
+++ app/src/main/java/com/schoolandroidexample/permissionrequestdemo/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.schoolandroidexample.permissionrequestdemo
+
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.schoolandroidexample.permissionrequestdemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    companion object {
+        const val REQUEST_CALL_STATE = 1001
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.btnCall.setOnClickListener {
+            intentCall()
+        }
+
+        //requestPermission()
+    }
+
+
+
+
+    fun intentCall() {
+        requestPermission()
+        val uri ="tel:" + binding.etPhone.text
+        val intentPackage = Intent(Intent.ACTION_CALL, Uri.parse(uri))
+
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+            startActivity(intentPackage)
+        }
+    }
+
+
+
+    private fun requestPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (shouldShowRequestPermissionRationale(Manifest.permission.CALL_PHONE)) {
+                showRationaleDialog(
+                    "Engedély kérés",
+                    "Szükség van rá, mivel ezt teszteljük",
+                    Manifest.permission.CALL_PHONE,
+                    REQUEST_CALL_STATE
+                )
+            } else {
+                requestPermissions(
+                    arrayOf(Manifest.permission.CALL_PHONE),
+                    REQUEST_CALL_STATE
+                )
+            }
+        } else {
+            // Permission is already granted
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUEST_CALL_STATE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+    private fun showRationaleDialog(
+        title: String,
+        message: String,
+        permission: String,
+        requestCode: Int
+    ) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("Ok") { _, _ ->
+                requestPermissions(arrayOf(permission), requestCode)
+            }
+        builder.create().show()
+    }
+}
